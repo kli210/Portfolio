f25c5c6109fa2ee81b1d7e07efe9da57cb3ac30b
kli210 <kellyli2000.kl@gmail.com>
2023-11-20 17:59:29 -0500

    modify contact form submission

--- JS/contact-form.js
+++ JS/contact-form.js
@@ -22,23 +22,4 @@ document.addEventListener('DOMContentLoaded', function () {
         return true;
     }
 
-    function handleFormSubmission() {
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'submit_form.php', true);
-        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                if (xhr.responseText === 'success') {
-                    alert('Form submitted successfully!');
-                    // You can redirect the user or perform other actions here
-                } else {
-                    alert('Error submitting the form. Please try again later.');
-                }
-            }
-        };
-
-        var formData = new FormData(contactForm);
-        xhr.send(formData);
-    }
 });
